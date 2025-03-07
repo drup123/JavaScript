608d3bfbda93d016ad7029c5a1b8de000ef671a4
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-03-07 12:29:52 +0000

    Learned about Strings in javascript

--- Strings.js
+++ Strings.js
@@ -10,3 +10,22 @@ console.log(`Your name is ${Name} and your age is ${Age}`);
 const UserName = new String("Drup-Patil")      // STRING OBJ (USE RARELY)
 UserName.language = "English"
 console.log(UserName.language)                // WE CAN ADD PROPERTIES IN OBJ
+
+
+
+//----------------------- Methods of Strings -----------------------------------------------
+
+console.log(Name.charAt(3))
+
+console.log(Name.includes('d')) // checks char present or not
+
+console.log(Name.length)   // Its a property not method
+
+console.log(Name.replace('r','u'))
+
+const mail = "  druppatil   "
+console.log(mail.trim())   // remove unwanted spaces
+
+
+const Password = "Drup-patil"
+console.log(Password.split('-'))
