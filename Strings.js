4b2fce78b17d9bbb65c5d9a8ace3b185876b0b01
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-03-07 12:08:09 +0000

    Learned About Strings in Javascript

---
+++ Strings.js
@@ -0,0 +1,12 @@
+// Template Literals (or Template Strings) in JavaScript.
+const Name = "Drup"   // PRIMITIVE STRING 
+const Age = 21
+console.log(`Your name is ${Name} and your age is ${Age}`);
+
+
+
+//-------------------------------------------------------------------------------
+
+const UserName = new String("Drup-Patil")      // STRING OBJ (USE RARELY)
+UserName.language = "English"
+console.log(UserName.language)                // WE CAN ADD PROPERTIES IN OBJ
