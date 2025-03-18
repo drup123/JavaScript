5a2b245d0fd49c888405dcc3d4ac57b5ad8b542b
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-03-18 13:12:28 +0000

    config

---
+++ Basics02/IIFE.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression
+
+function Parent() {
+    let Mob = "965xxxxx"
+    console.log(Mob);
+    
+    function child() {
+        let Mob = "5333XX"
+        console.log(Mob)
+    } child()
+}
+Parent()
