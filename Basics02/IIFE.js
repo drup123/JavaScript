6d12d99b0fcd3d26279e5bb6d5f3b534ecb2800f
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-03-19 11:30:43 +0000

    Immediately Invoked Function Expression

--- Basics02/IIFE.js
+++ Basics02/IIFE.js
@@ -10,3 +10,44 @@ function Parent() {
     } child()
 }
 Parent()
+
+
+// ------------------- Case 1 -> Avoiding Global Variable Pollution ---------------------------
+
+let AccId = 2
+function Modifie() {
+    AccId++      // Here AccId(global) changed
+}
+Modifie()
+console.log(AccId)
+
+const id = (() => {
+    let Userid = 2
+    function Cant_Modifie() {
+        Userid++
+    }
+    Cant_Modifie()
+})()
+
+console.log(id.Userid)  // Give ERROR we can't access Userid
+
+
+//----------------------- Case 2 -> Encapsulating Private Variables & Preventing Modification-----------------------------
+
+let Firstobj = {
+    val : 0,
+    Name : "Drup"
+}
+
+Firstobj.Name = "Patil"
+
+console.log(Firstobj.Name)
+
+
+const Func = (() => {
+    let surname = "Patil"
+})()
+
+Func.surname = "Drup"
+console.log(Func.surname)      // Give ERROR we can't access
+
