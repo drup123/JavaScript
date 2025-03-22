ee05c21816e251a2fbb2afc011f42b9017ee4035
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-03-22 11:48:05 +0000

    Higher loops

--- Basics02/Higher_loops.js
+++ Basics02/Higher_loops.js
@@ -1,58 +1,92 @@
-// ---------------------- for..of loop ----------------------
+// // ---------------------- for..of loop ----------------------
 
-const Marvel = ["Captain America","IronMan","Thor","Doctor Strange"]
+// const Marvel = ["Captain America","IronMan","Thor","Doctor Strange"]
 
-for (const i of Marvel) {
-    console.log(i)
-}
+// for (const i of Marvel) {
+//     console.log(i)
+// }
 
-// Object
-const obj = {
-    a : 1,
-    b : 2,
-    c : 3
-}
-for (const [key,val] of obj) {
-    console.log(key, " :- ", val)          // TypeError: obj is not iterable
-}
-
-
-// Map using for....of 
-const map = new Map()
-map.set('a',1)
-map.set('b',2)
-map.set('c',3)
-map.set('d',4)
-
-for (const [key,val] of map) {
-    console.log(`${key} :- ${val}`)
-}
+// // Object
+// const obj = {
+//     a : 1,
+//     b : 2,
+//     c : 3
+// }
+// for (const [key,val] of obj) {
+//     console.log(key, " :- ", val)          // TypeError: obj is not iterable
+// }
 
 
-// ---------------------------- for..in ---------------------------------
+// // Map using for....of 
+// const map = new Map()
+// map.set('a',1)
+// map.set('b',2)
+// map.set('c',3)
+// map.set('d',4)
 
-// Object
-const Rollno = {
-    39 : "Drup",
-    36 : "Pawan",
-    31 : "Kalesh"
-}
-for (const key in Rollno) {
-    console.log(`${key} :- ${Rollno[key]}`)
-}
+// for (const [key,val] of map) {
+//     console.log(`${key} :- ${val}`)
+// }
 
-// Array
-const DC = ["Superman","Wonder-Woman","Flash"]
-for (const key in DC) {
-    console.log(DC[key])
-}
 
-//Map
-const map2 = new Map()
-map2.set(1,"Spider-man")
-map2.set(2,"Scarlet witch")
-map2.set(3,"Hulk")
+// // ---------------------------- for..in ---------------------------------
 
-for (const key in map2) {
-    console.log(key)
-}
+// // Object
+// const Rollno = {
+//     39 : "Drup",
+//     36 : "Pawan",
+//     31 : "Kalesh"
+// }
+// for (const key in Rollno) {
+//     console.log(`${key} :- ${Rollno[key]}`)
+// }
+
+// // Array
+// const DC = ["Superman","Wonder-Woman","Flash"]
+// for (const key in DC) {
+//     console.log(DC[key])
+// }
+
+// //Map
+// const map2 = new Map()
+// map2.set(1,"Spider-man")
+// map2.set(2,"Scarlet witch")
+// map2.set(3,"Hulk")
+
+// for (const key in map2) {
+//     console.log(key)
+// }
+
+
+// ------------------------ for each ------------------------------------
+
+// const arr = ["Deadpool","Wolverine","Storm","Quick Silver"]
+
+// arr.forEach((elements) => {
+//     console.log(elements) 
+// });
+
+// arr.forEach((elements, index, Fullarray) => {
+//     console.log(`${index} , ${elements} , ${Fullarray}`)
+// })
+
+
+// Objects in array
+const info = [
+    {
+        Name : "Steve Rogers",
+        Power : "Super Soldier"
+    },
+    {
+        Name : "Tony Stark",
+        Power : "Technology" 
+    },
+    {
+        Name : "Thor",
+        Power : "Ligthning"
+    }
+]
+
+info.forEach((element) => {
+    console.log(`${element.Name}'s power is ${element.Power}`)
+})
